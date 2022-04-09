fac2e0fec0ff03dcdb02773e998381813fec94e1
Taner Özer <tanerozer16@gmail.com>
2022-04-10 00:48:03 +0300

    php_userlogin

---
+++ Php_User_Login/README.md
@@ -0,0 +1,2 @@
+# Php_User_Login
+Php_User_Login website
---
+++ Php_User_Login/User_login_PHP/hataligiris.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hatali giris</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<?php
+	echo "Kullanıcı Adı veya Şifre Yanlış <br>";	
+	?>
+
+	<hr>
+	<a href="index.html">Giriş Ekranına Geri Dön</a>
+
+
+</body>
+</html>
