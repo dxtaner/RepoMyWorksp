a9194b0b75bea507d8985342d21dc1a45102f856
Taner Özer <tanerozer16@gmail.com>
2022-08-16 14:19:30 +0300

    Update Sql_5.sql

--- Sql_5.sql
+++ Sql_5.sql
@@ -1,14 +1,8 @@
---film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
-select distinct(replacement_cost) from film;
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+select title from film where title like '%n' order by length desc limit 5;
 
---film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
-select count(distinct(replacement_cost)) from film;
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+select title from film where title like '%n' order by length asc offset 5 limit 5;
 
---film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
-select count(*) from film where title like 'T%' and rating='G';
-
---country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
-select count(*) from country where country like '_____';
-
---city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
-select count(*) from city where city Ilike '%r';
+--customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+select last_name, store_id from customer where store_id = 1 order by last_name desc limit 4;
