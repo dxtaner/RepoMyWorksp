a4d2073286ae31f0825caf36cd9c771aebb7a8e3
Taner Özer <tanerozer16@gmail.com>
2022-08-18 16:16:44 +0300

    Create Sql_10.sql

---
+++ Sql_10.sql
@@ -0,0 +1,8 @@
+-- city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+select city.city, country.country from city Left JOIN country on city.country_id=country.country_id;  
+
+-- customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+select payment.payment_id, customer.first_name, customer.last_name from customer RIGHT JOIN payment on payment.customer_id=customer.customer_id;  
+
+-- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız
+select rental.rental_id, customer.first_name, customer.last_name from rental Full JOIN customer on rental.customer_id=customer.customer_id;  
